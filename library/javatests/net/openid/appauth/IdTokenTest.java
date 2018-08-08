91dc40394ec139cff8106c787e0fb070adc76af4
Øyvind Robertsen <oyvind.robertsen@eika.no>
2018-08-08 17:21:07 +0200

    test: better coverage for ID Token parsing

--- library/javatests/net/openid/appauth/IdTokenTest.java
+++ library/javatests/net/openid/appauth/IdTokenTest.java
@@ -3,7 +3,10 @@ package net.openid.appauth;
 import android.support.annotation.Nullable;
 import android.util.Base64;
 
+import java.util.Arrays;
+import java.util.List;
 import net.openid.appauth.IdToken.IdTokenException;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
@@ -27,16 +30,33 @@ public class IdTokenTest {
 
     @Test
     public void testFrom() throws Exception {
-        String serializedIdToken = getUnsignedIdToken(
+        String testToken = getUnsignedIdToken(
             TEST_ISSUER,
             TEST_SUBJECT,
             TEST_AUDIENCE,
-            TEST_NONCE);
-        IdToken idToken = IdToken.from(serializedIdToken);
-        assertEquals(idToken.issuer, TEST_ISSUER);
-        assertEquals(idToken.subject, TEST_SUBJECT);
+            TEST_NONCE
+        );
+        IdToken idToken = IdToken.from(testToken);
+        assertEquals(TEST_ISSUER, idToken.issuer);
+        assertEquals(TEST_SUBJECT, idToken.subject);
         assertThat(idToken.audience, contains(TEST_AUDIENCE));
-        assertEquals(idToken.nonce, TEST_NONCE);
+        assertEquals(TEST_NONCE, idToken.nonce);
+    }
+
+    @Test
+    public void testFrom_shouldParseAudienceList() throws Exception {
+        List<String> audienceList = Arrays.asList(TEST_AUDIENCE, "AUDI3NCE2");
+        String testToken = getUnsignedIdTokenWithAudienceList(
+            TEST_ISSUER,
+            TEST_SUBJECT,
+            audienceList,
+            TEST_NONCE
+        );
+        IdToken idToken = IdToken.from(testToken);
+        assertEquals(TEST_ISSUER, idToken.issuer);
+        assertEquals(TEST_SUBJECT, idToken.subject);
+        assertEquals(audienceList, idToken.audience);
+        assertEquals(TEST_NONCE, idToken.nonce);
     }
 
     @Test(expected = IdTokenException.class)
@@ -51,12 +71,80 @@ public class IdTokenTest {
 
     @Test(expected = JSONException.class)
     public void testFrom_shouldFailOnMissingIssuer() throws IdTokenException, JSONException {
-
-        IdToken.from("header.claims");
+        String testToken = getUnsignedIdToken(
+            null,
+            TEST_SUBJECT,
+            TEST_AUDIENCE,
+            TEST_NONCE
+        );
+        IdToken.from(testToken);
     }
 
+    @Test(expected = JSONException.class)
+    public void testFrom_shouldFailOnMissingSubject() throws IdTokenException, JSONException {
+        String testToken = getUnsignedIdToken(
+            TEST_ISSUER,
+            null,
+            TEST_AUDIENCE,
+            TEST_NONCE
+        );
+        IdToken.from(testToken);
+    }
+
+    @Test(expected = JSONException.class)
+    public void testFrom_shouldFailOnMissingAudience() throws IdTokenException, JSONException {
+        String testToken = getUnsignedIdToken(
+            TEST_ISSUER,
+            TEST_SUBJECT,
+            null,
+            TEST_NONCE
+        );
+        IdToken.from(testToken);
+    }
+
+    @Test(expected = JSONException.class)
+    public void testFrom_shouldFailOnMissingExpiration() throws IdTokenException, JSONException {
+        String testToken = getUnsignedIdToken(
+            TEST_ISSUER,
+            TEST_SUBJECT,
+            TEST_AUDIENCE,
+            null,
+            0L,
+            TEST_NONCE
+        );
+        IdToken.from(testToken);
+    }
+
+    @Test(expected = JSONException.class)
+    public void testFrom_shouldFailOnMissingIssuedAt() throws IdTokenException, JSONException {
+        String testToken = getUnsignedIdToken(
+            TEST_ISSUER,
+            TEST_SUBJECT,
+            TEST_AUDIENCE,
+            0L,
+            null,
+            TEST_NONCE
+        );
+        IdToken.from(testToken);
+    }
     private static String base64UrlNoPaddingEncode(byte[] data) {
         return Base64.encodeToString(data, Base64.URL_SAFE | Base64.NO_PADDING | Base64.NO_WRAP);
+    }
+
+    static String getUnsignedIdTokenWithAudienceList(
+        @Nullable String issuer,
+        @Nullable String subject,
+        @Nullable List<String> audience,
+        @Nullable String nonce) {
+        Long nowInSeconds = SystemClock.INSTANCE.getCurrentTimeMillis() / 1000;
+        Long tenMinutesInSeconds = (long) (10 * 60);
+        return getUnsignedIdToken(
+            issuer,
+            subject,
+            audience,
+            nowInSeconds + tenMinutesInSeconds,
+            nowInSeconds,
+            nonce);
     }
 
     static String getUnsignedIdToken(
@@ -73,6 +161,30 @@ public class IdTokenTest {
             nowInSeconds + tenMinutesInSeconds,
             nowInSeconds,
             nonce);
+    }
+
+    static String getUnsignedIdToken(
+        @Nullable String issuer,
+        @Nullable String subject,
+        @Nullable List<String> audience,
+        @Nullable Long expiration,
+        @Nullable Long issuedAt,
+        @Nullable String nonce) {
+        JSONObject header = new JSONObject();
+        JsonUtil.put(header, "typ", "JWT");
+
+        JSONObject claims = new JSONObject();
+        JsonUtil.putIfNotNull(claims, "iss", issuer);
+        JsonUtil.putIfNotNull(claims, "sub", subject);
+        JsonUtil.put(claims, "aud", new JSONArray(audience));
+        JsonUtil.putIfNotNull(claims, "exp", expiration != null ? String.valueOf(expiration) : null);
+        JsonUtil.putIfNotNull(claims, "iat", issuedAt != null ? String.valueOf(issuedAt) : null);
+        JsonUtil.putIfNotNull(claims, "nonce", nonce);
+
+
+        String encodedHeader = base64UrlNoPaddingEncode(header.toString().getBytes());
+        String encodedClaims = base64UrlNoPaddingEncode(claims.toString().getBytes());
+        return encodedHeader + "." + encodedClaims;
     }
 
     static String getUnsignedIdToken(
