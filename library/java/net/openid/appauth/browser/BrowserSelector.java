8a357aa9e5c894bcc27aa416a0780bc4c4d4e89b
Henning Nielsen Lund <henning.n.lund@jp.dk>
2020-06-14 11:05:10 +0200

    Fix linting errors.

--- library/java/net/openid/appauth/browser/BrowserSelector.java
+++ library/java/net/openid/appauth/browser/BrowserSelector.java
@@ -182,7 +182,8 @@ public final class BrowserSelector {
     private static boolean isFullBrowser(ResolveInfo resolveInfo) {
         // The filter must match ACTION_VIEW, CATEGORY_BROWSEABLE, and at least one scheme,
         if (!resolveInfo.filter.hasAction(Intent.ACTION_VIEW)
-                || !(resolveInfo.filter.hasCategory(Intent.CATEGORY_BROWSABLE) || (resolveInfo.filter.hasCategory(Intent.CATEGORY_APP_BROWSER)))
+                || !(resolveInfo.filter.hasCategory(Intent.CATEGORY_BROWSABLE)
+                    || (resolveInfo.filter.hasCategory(Intent.CATEGORY_APP_BROWSER)))
                 || resolveInfo.filter.schemesIterator() == null) {
             return false;
         }
